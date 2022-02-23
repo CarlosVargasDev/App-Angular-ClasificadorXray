6d59c07bd6f8b85e8d34f1a4de7af8cad54b6a26
Carlos Vargas <Carlos.Vargas.Dev@gmail.com>
2022-02-23 14:27:39 -0600

    API: Configurando CORS

--- API/app.py
+++ API/app.py
@@ -1,16 +1,20 @@
 from flask import Flask, jsonify, request
 from modelo.clasificador import Resnet18Covid
 from PIL import Image
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+
 
 #########################################
 # Rutas
 #########################################
 # Testing Route
-@app.route('/', methods=['GET'])
+@app.route('/ping', methods=['GET'])
 def ping():
-    return jsonify({'response': 'Bienvenido a la API!'})
+    return jsonify({"status":"ok",'response': 'Bienvenido a la API!'})
 
 # Ruta UploadImages
 @app.route('/xray_image', methods=['POST'])
